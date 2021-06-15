c37ce326ed3b17573555ba23c8fbc4cb6fd4004f
Florent-Laptop <florentgallou@gmail.com>
2021-06-16 01:25:49 +0200

    wireframes class diagram bootstrap

--- view/indexView.php
+++ view/indexView.php
@@ -1,1 +1,36 @@
-<p>Vos livre au catalogue s'affichent sur cette page</p>
+<?php
+    require "view/template/header.php";
+    require "view/template/nav.php";
+?>
+
+<main class="my-5">
+    <h1 class="text-center m-5">Liste des Livres</h1>
+    <div class="container my-5">
+        <table class="table my-5">
+            <thead class="table-dark">
+                <tr>
+                    <th scope="col">Titre</th>
+                    <th scope="col">Auteur</th>
+                    <th scope="col">Date d'édition</th>
+                    <th scope="col">Éditeur</th>
+                    <th scope="col">Catégorie</th>
+                    <th scope="col">Disponible</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">Titre 1</th>
+                    <td>Auteur 1</td>
+                    <td>24/12/2021</td>
+                    <td>Éditeur 1</td>
+                    <td>Catégorie 1</td>
+                    <td>oui</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</main>
+
+<?php
+    require "view/template/footer.php";
+?>
---
+++ view/template/nav.php
@@ -0,0 +1,21 @@
+<div class="container-fluid col-6 col-sm-12 bg-light">
+        <nav class="navbar navbar-expand-lg navbar-light border">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#"></a>
+                <button class="navbar-toggler bg-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse bg-light px-1 px-sm-0">
+                    <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+                        <li class="nav-item text-uppercase">
+                            <a class="nav-link text-primary" href="index.php">Livres</a>
+                        </li>
+                        <li class="nav-item text-uppercase">
+                            <a class="nav-link text-primary" href="borrower.php">Usagers</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+</header>
